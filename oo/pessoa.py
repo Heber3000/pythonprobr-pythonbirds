9b95086f97234b143a2e50db9e607ece99a90bfe
heber3000 <heberlevy@gmail.com>
2020-09-23 18:07:21 -0300

    criado atributo complexo: filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=30):
+    def __init__(self, *filhos, nome=None, idade=30):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Heber')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Levy'
-    print(p.nome)
-    print(p.idade)
+    levy = Pessoa(nome='Levy')
+    heber = Pessoa(levy, nome='Heber')
+    print(Pessoa.cumprimentar(heber))
+    print(id(heber))
+    print(heber.cumprimentar())
+    print(heber.nome)
+    print(heber.idade)
+    for filho in heber.filhos:
+        print(filho.nome)
